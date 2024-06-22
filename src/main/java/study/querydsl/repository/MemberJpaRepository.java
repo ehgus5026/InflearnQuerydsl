1dc9ee7695d3900a9e210d95e137f90ea360595d
idohyeon <ehgus5026@naver.com>
2024-06-22 23:25:13 +0900

    -JPA + Querydsl

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,6 +1,8 @@
 package study.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,8 @@ import study.querydsl.entity.QTeam;
 import java.util.List;
 import java.util.Optional;
 
+import static io.micrometer.common.util.StringUtils.isEmpty;
+import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.*;
 import static study.querydsl.entity.QTeam.*;
 
@@ -70,17 +74,17 @@ public class MemberJpaRepository {
                 .fetch();
     }
 
-    // 빌더로 동적 쿼리 + 성능 최적화 + DTO로 조회
+    // 빌더로 동적 쿼리 + 성능 최적화 + DTO로 조회 (생성자 + @QueryProjection)
     // 참고: `@QueryProjection` 을 사용하면 해당 DTO가 Querydsl을 의존하게 된다.
     // 이런 의존이 싫으면, 해당 어노테이션을 제거하고, `Projection.bean(), fields(), constructor()` 을 사용하면 된다.
-    // 조건을 하나라도 넣어줘야 함. 데이터가 적을 땐 상관이 없지만 조건이 하나라도 없으면 모든 데이터를 다 끌어와서 조회하게 돼서 양이 많아지면 성능 저하됨.
+    // BooleanBuilder에 조건을 하나라도 넣어줘야 함. 데이터가 적을 땐 상관이 없지만 조건이 하나라도 없으면 모든 데이터를 다 끌어와서 조회하게 돼서 양이 많아지면 성능 저하됨.
     public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
         BooleanBuilder builder = new BooleanBuilder();
 
-        if (StringUtils.hasText(condition.getUsername())) {
+        if (hasText(condition.getUsername())) {
             builder.and(member.username.eq(condition.getUsername()));
         }
-        if (StringUtils.hasText(condition.getTeamName())) {
+        if (hasText(condition.getTeamName())) {
             builder.and(team.name.eq(condition.getTeamName()));
         }
         if (condition.getAgeGoe() != null) {
@@ -101,6 +105,43 @@ public class MemberJpaRepository {
                 .leftJoin(member.team, team)
                 .where(builder)
                 .fetch();
+    }
+
+    // 일단 이 방식을 권장.(where를 하나로 묶지 말고, 다른 곳에서도 쓸 수 있게)
+    // 반환 타입이 Member로 바껴도 queryFactory 문만 수정 해주면 사용 가능.
+    public List<MemberTeamDto> searchByWhere(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .fetch();
+
+    }
+
+    // StringUtils.hasText()로 해도 됨.
+    // 빈환 타입을 BooleanExpression으로 해두면 and로 where 연결 가능
+    private BooleanExpression usernameEq(String username) {
+        return isEmpty(username) ? null : member.username.eq(username);
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
     }
 
 }
