606fec4b81efdaca6c4fb5501ade537c4d06e3b2
idohyeon <ehgus5026@naver.com>
2024-06-22 23:25:22 +0900

    -JPA + Querydsl Test

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -66,7 +66,8 @@ class MemberJpaRepositoryTest {
         condition.setAgeLoe(40);
         condition.setTeamName("teamB");
 
-        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+//        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        List<MemberTeamDto> result = memberJpaRepository.searchByWhere(condition);
 
         assertThat(result).extracting("username").containsExactly("member3", "member4");
     }
