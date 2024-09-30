ba8f522e82c088d545b7b398d7501200d03ccd47
idohyeon <ehgus5026@naver.com>
2024-09-30 19:46:42 +0900

    -Spring Data Jpa Test

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package study.querydsl.repository;
 
 import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import java.util.List;
@@ -97,5 +99,34 @@ class MemberRepositoryTest {
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
     }
 
+    /**
+     * 한계점 : 조인X (묵시적 조인은 가능하지만 left join이 불가능하다.), 하나의 간단한 테이블에서는 사용 괜찮.
+     * 클라이언트가 Querydsl에 의존해야 한다. 서비스 클래스가 Querydsl이라는 구현 기술에 의존해야 한다.
+     * 복잡한 실무환경에서 사용하기에는 한계가 명확하다.
+     * 참고 : 'QuerydslPredicateExecutor'는 Pageable, Sort를 모두 지원하고 정상 동작한다.
+     */
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
 
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
+    }
 }
